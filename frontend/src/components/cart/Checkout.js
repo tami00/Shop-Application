7898b72655c5af1f4fd7b0b2853341e69950598b
Tami Adeduntan <tami.adeduntan@gmal.com>
2022-05-09 04:53:50 +0100

    loyalty code

--- frontend/src/components/cart/Checkout.js
+++ frontend/src/components/cart/Checkout.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ShoppingCartOutlined, DeleteOutlined } from '@ant-design/icons'
-import { Card, Space,Input  } from 'antd'
+import { Card, Space, Input, Button, message } from 'antd'
 import authService from "../../services/auth.service";
 import { useHistory } from "react-router-dom";
 
 const { Meta } = Card;
 
 function Checkout() {
+    const [loyalty, setLoyalty] = useState("");
     const currentUser = authService.getCurrentUser();
     const [cartDetails, setCartDetails] = useState([])
+    const [total, setTotal] = useState([])
     const history = useHistory();
+
+    const handleChangeLoyalty = ( event ) => {
+        setLoyalty(event.currentTarget.value)
+    }
+
+    const error = () => {
+        message.error('Enter a valid discount');
+      };
 
     useEffect(() => {
 
@@ -21,7 +31,7 @@ function Checkout() {
     const checkout = () => {
         axios.get("http://localhost:8080/api/getCart", { data: currentUser.id })
             .then((response) => {
-                console.log('DATA', response.data);
+                console.log('DATA', response.data.products.items);
                 setCartDetails(response.data.products)
             })
             .catch((err) => {
@@ -30,8 +40,41 @@ function Checkout() {
     };
 
     const items = cartDetails.items
-    console.log('ITEMS', items)
+    // console.log('ITEMS', items)
 
+    // const map =items
+
+    // const result = Object && Object.values(map);
+
+    // console.log(result)
+
+    const variables = {
+        title: cartDetails.items,
+        bill: cartDetails.bill,
+        userID: currentUser.id
+    }
+
+    const createOrder = () => {
+        axios.post("http://localhost:8080/api/order/addOrder", { data: variables })
+            .then((response) => {
+                // console.log('DATA', response.data);
+                // setCartDetails(response.data.products)
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const getDiscount = () =>{
+        if(loyalty != "NEW10") {
+            error()
+        }
+        else{
+            const percent = cartDetails.bill * 0.1
+            setTotal(cartDetails.bill - percent)
+            console.log(total)
+        }
+    }
 
     return (
         <div style={{ width: '75%', margin: '3rem auto' }}>
@@ -48,30 +91,40 @@ function Checkout() {
                     <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
                         {items ? items.map((product, index) => {
 
+
                             return <Card
-                                    // key={index}
-                                    // productTitle={product.title}
-                                    hoverable={true}
-                                >
-                                    <Meta
-                                        title={product.title}
+                                // key={index}
+                                // productTitle={product.title}
+                                hoverable={true}
+                            >
+                                <Meta
+                                    title={product.title}
                                     description={`$${product.price}`}
-                                    />
-                                     <DeleteOutlined/>
-                                </Card>
-            
+                                />
+                                <DeleteOutlined />
+                            </Card>
+
                         }) : <div></div>}
-</Space>
+                    </Space>
 
 
-<br></br>
-<h3>Total: {cartDetails.bill}</h3>
-<p>Input any discount codes below</p>
-<Input style={{ width: '20%' }}/>
-
+                    <br></br>
+                    {total.length === 0 ? <h3>Total: {cartDetails.bill}</h3> : <h3>Total: {total}</h3>} 
+                    <p>Input any discount codes below</p>
+                    <Input
+                        style={{ width: '20%' }}
+                        onChange={handleChangeLoyalty}
+                        value={loyalty}
+                    />
+                    <br></br>
+                    <Button onClick={getDiscount}>Enter</Button>
+                    <br></br>
+                    <br></br>
+                    <Button onClick={createOrder} size="large" shape="round" icon={<ShoppingCartOutlined />}>BUY</Button>
+                    <br></br>
+                    <br></br>
                 </div>
             }
-
         </div>
     )
 }
